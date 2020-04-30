0bc4dcc6d45a9d18aa108b935a90c7c64c73c16d
Ihar Dubadzelau <ihar@dubadzelau.xyz>
2020-04-30 02:47:06 -0400

    1. Creating a catalog and products. (data transfer between the components: props & v-bind)

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
+    <p>Hello Vue CLI !!!</p>
 
+    <v-main-wrapper></v-main-wrapper>
   </div>
 </template>
 
 <script>
+import vMainWrapper from './components/v-main-wrapper'
+
+
+
 export default {
   name: 'App',
   components: {
+    vMainWrapper
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="v-main-wrapper">
+        <p>{{title}}</p>
+
+        <hr>
+        <v-catalog />
+        <hr>
+        <v-cart />
+    </div>
+</template>
+
+<script>
+import vCatalog from './v-catalog';
+import vCart from './v-cart';
+
+export default {
+    //Имя компонента - обязательный параметр
+    name: "v-main-wrapper",
+    //Дети этого компонента
+    components: {
+        vCatalog,
+        vCart
+    },
+    //то что прилетает из родителя
+    props: {},
+    //возращаемые данные этого компонента - персональные данные
+    data() {
+        return{
+            title: 'Main wrapper'
+        }
+    },
+    //вычислительные св-ва компонента
+    computed: {},
+    //здесь отлавдиваются действия пользователя
+    //различные клики, показы модальных окон, скрытие показ элементов, анимации и т.д.
+    methods: {},
+    //здесь мы следим, отлавливаем изменения, что происходит в компоненте
+    watch: {},
+//--- хуки жизненого цикла ---
+    //отрабатывает когда компонент полностью загрузится - один из основных
+    mounted() {
+        console.log('hook - mounted');
+    }
+}
+</script>
+
+<style>
+ .v-main-wrapper {
+     max-width: 900px;
+     margin: 0 auto;
+ }
+</style>
